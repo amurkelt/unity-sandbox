e59ed34b139556ac5bc1f32802d018fb6cb45bf8
Andrei Taras <125547493+amurkelt@users.noreply.github.com>
2026-02-24 00:01:01 +0200

    feat: add test Particle System prefabs

---
+++ Assets/Scripts/Character/Enemy.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem damageParticles;
+    private ParticleSystem damageParticleInstance;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        SpawnDamageParticles();
+    }
+
+    public void Die()
+    {
+        SpawnDamageParticles();
+    }
+
+    [ContextMenu("SpawnDamageParticles")]
+    private void SpawnDamageParticles()
+    {
+        float topOfSprite = spriteRenderer.bounds.max.y;
+
+        Vector3 spawnPosition = new Vector3(
+            transform.position.x,
+            topOfSprite + 2.5f, // adjust the offset if needed
+            transform.position.z
+        );
+
+        damageParticleInstance = Instantiate(damageParticles, spawnPosition, Quaternion.identity);
+    }
+}

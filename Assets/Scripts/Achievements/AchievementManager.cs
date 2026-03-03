7f17c209254a69f37fcf0ac901b58e360c5a1335
Andrei Taras <125547493+amurkelt@users.noreply.github.com>
2026-03-04 00:41:19 +0200

    feat: add Placeholder actions per achievement

--- Assets/Scripts/Achievements/AchievementManager.cs
+++ Assets/Scripts/Achievements/AchievementManager.cs
@@ -68,9 +68,92 @@ public class AchievementManager : MonoBehaviour
     }
 
     private void Unlock(AchievementData data)
-    {
+    {       
         SaveManager.UnlockAchievement(data.id);
         popup.Show(data.title, data.description);
+
+        // ----------------- Placeholder actions per achievement -----------------
+        switch (data.id)
+        {
+            case "first_object":
+                // TODO: AchievementSystem call
+                Debug.Log($"Unlocked achievement {data.id}!");
+                break;
+
+            case "find_10":                
+                break;
+
+            case "find_25":
+                break;
+
+            case "find_50":
+                break;
+
+            case "find_75":
+                break;
+
+            case "find_300":
+                break;
+
+            case "find_400":
+                break;
+
+            case "rooftop_obj":
+                break;
+
+            case "window_obj":
+                break;
+
+            case "rookie_01":
+                // Paint 50
+                break;
+
+            case "tree_obj":
+                break;
+
+            case "bushes_obj":
+                break;
+
+            case "complete_2_levels":
+                break;
+
+            case "all_level1":
+                break;
+
+            case "all_level2":
+                break;
+
+            case "all_level3":
+                break;
+
+            case "all_level4":
+                break;
+
+            case "level1_10min":
+                break;
+
+            case "level2_5min":
+                break;
+
+            case "level3_5min":
+                break;
+
+            case "level4_5min":
+                break;
+
+            case "one_hour":
+                break;
+
+            case "complete_all_levels":
+                break;
+
+            case "complete_all_achievements":
+                break;
+
+            default:
+                Debug.LogWarning($"Unlocked achievement {data.id} has no placeholder logic.");
+                break;
+        }
     }
 
     // ---------------- HELPERS ----------------
